8181a4b28696822816983dde6459f54e48678d5f
Julio Gragirena <juliogragirena@outlook.es>
2021-01-04 22:21:15 -0400

    forEach arrays render example

---
+++ blockbuster/js/render.js
@@ -0,0 +1,32 @@
+function buildElement({ title, id, poster_path, vote_average }) {
+  const $template = document.createElement("article");
+  $template.classList.add("movie");
+  const URL = `https://image.tmdb.org/t/p/w220_and_h330_face${poster_path}`;
+  $template.innerHTML = `
+        <img
+          class="movie-poster"
+          src=${URL}
+          alt="${title}"
+        />
+        <p class="movie-title">${title}</p>
+        <p class="movie-id">${id}</p>
+        <span class="movie-rate">${vote_average}</span>
+    `;
+  return $template;
+}
+
+function cleanMovieList() {
+  const $container = document.getElementById("container");
+  $container.innerHTML = "";
+}
+
+function renderElement(movie) {
+  const $container = document.getElementById("container");
+  const element = buildElement(movie)
+  $container.append(element);
+}
+
+export default function renderMovieList(list) {
+  cleanMovieList();
+  list.forEach(renderElement);
+}
